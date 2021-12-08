af9608adeda2dddaacf3b97c044b2cfc0cd00b91
Cody <relationalprogrammer@gmail.com>
2021-12-08 16:07:32 -0600

    all tests passing

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -365,14 +365,16 @@ function init(data) {
     // Complete this function
     const rateAndExit = () => {
         // Create a new instance of Rating and assign it to a variable named playerRating here
+        let playerRating = new Rating();
         rl.question("How would you rate this game on a scale of 1-10 (1 being the lowest)?:", function (r) {
             if (r == "" || isNaN(r) || r == 0 || r > 10) {
                 console.log("Invalid rating! Please nter a number from 1 - 10".red);
                 rateAndExit();
             } else {
                 // Call rating setter method of playerRating to set user entered rate value here
-                
+                playerRating.rating = r
                 // Call Object.assign() method here to populate target
+                let target = Object.assign({}, player, playerRating);
                 
                 console.log(`${target.name} you rated this game as ${target.rate}`.green);
                 console.log("Thank you for your valuable feedback.".blue);
@@ -383,14 +385,14 @@ function init(data) {
 
     // Complete this function
     const exitLost = () => {
-        let pointsToReach; // Assign calculated value to pointsToReach here
+        let pointsToReach = 500 - player.getCurrentScore(); // Assign calculated value to pointsToReach here
         console.log(`Your chances are over! You are short of ${pointsToReach} to become a Shopping Master. Good Luck for next time!`.yellow);
         rateAndExit();
     };
 
     // Complete this function
     const exitWon = () => {
-        let finalStatus; 
+        let finalStatus = player.status; 
         console.log(`Congratulations!!! You became ${finalStatus}!`.blue);
         rateAndExit();
     };
