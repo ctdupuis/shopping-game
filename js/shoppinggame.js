2e2c8a180dcf509fa283fca6303a4d2d62d4ee6f
ctdupuis <ctdupuis7@gmail.com>
2021-12-08 14:35:19 -0600

    test 10 passing

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -58,6 +58,23 @@ function MagicProduct(id, name, price, expiryDate, points, isBonus) {
 MagicProduct.prototype = Object.create(Product.prototype)
 
 // Define Rating class here
+class Rating {
+    constructor() {
+        this.rate = "";
+    }
+
+    set rating(value) {
+        if (value > 1 && value <= 4) {
+            this.rate = "OK"
+        } else if (value >= 5 && value <= 7) {
+            this.rate = "GOOD"
+        } else if (value > 7) {
+            this.rate = 'EXCEPTIONAL'
+        } else {
+            this.rate = 'BAD'
+        }
+    }
+}
 
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
