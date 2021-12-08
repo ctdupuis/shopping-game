c128f09d55ca229dd3ed5f813e77e8bf26ca2aa9
Cody <relationalprogrammer@gmail.com>
2021-12-08 15:52:21 -0600

    halfway through module 5

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -296,6 +296,7 @@ function init(data) {
 
         rl.question("What's your name? ", function (name) {
             // Assign the player object's name property to the user entered name here
+            player.name = name;
             console.log(`Welcome ${player.name} !!!`.blue);
             start(data);
         });
@@ -318,8 +319,8 @@ function init(data) {
     const shop = (prodList, tBill, lastProd) => {
         let totalBill = tBill;
         const prId = generateProductId();
-        let product = null; // Assign the value of product here
-        let productDetails = null; // Assign the value of productDetails here
+        let product = Object.is(lastProd, undefined) ? getProduct(prodList, prId) : lastProd; // Assign the value of product here
+        let productDetails = product.getDetails(); // Assign the value of productDetails here
 
         rl.question(`You can buy - ${productDetails}.\n Do you want to buy this item <Y/N>? `.yellow, function (option) {
             const regexYes = null; // Use the RegExp built-in object type here as appropriate
